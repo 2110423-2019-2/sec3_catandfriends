8b430a6faad45e16f8191f84a62a517eb4146a14
karnkittik <karnkitti_tang@hotmail.com>
2020-02-09 12:41:23 +0700

    add bootstap

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import VerifyCard from './components/VerifyCard'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<VerifyCard />, document.getElementById('root'));
