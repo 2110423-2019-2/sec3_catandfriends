d0fc804d4e1b6f715acb96f34457567aaaa57f85
karnkittik <karnkitti_tang@hotmail.com>
2020-02-09 12:41:52 +0700

    finish verifycard layout

--- frontend/src/components/VerifyCard.js
+++ frontend/src/components/VerifyCard.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import './VerifyCard.css'
 export default class VerifyCard extends Component {
   constructor(props) {
     super(props);
@@ -16,25 +17,37 @@ export default class VerifyCard extends Component {
     });
   };
   onClickHandler = () => {
-    const data = new FormData()
-    data.append('file', this.state.selectedFile)
-    axios.post("http://localhost:8000/upload", data, { 
-       // receive two    parameter endpoint url ,form data
-   }).then(res => { // then print response status
-    console.log(res.statusText)
- })
-}
+    const data = new FormData();
+    data.append("file", this.state.selectedFile);
+    axios
+      .post("http://localhost:8000/upload", data, {
+        // receive two    parameter endpoint url ,form data
+      })
+      .then(res => {
+        // then print response status
+        console.log(res.statusText);
+      });
+  };
   render() {
     return (
       <div>
-        <input type="file" name="file" onChange={this.onChangeHandler} />
-        <button
-          type="button"
-          className="btn btn-success btn-block"
-          onClick={this.onClickHandler}
-        >
-          Upload
-        </button>
+          <table>
+            <tbody>
+            <tr>
+            <td colSpan="2" style={{textAlign:"center"}}>
+                <label htmlFor="veridoc">Verification Document</label>
+            </td>
+            </tr>
+            <tr>
+                <td>
+                  <input id="veridoc"  className="form-control-file"  type="file" name="file" onChange={this.onChangeHandler}/>
+                </td>
+                <td>
+                  <button type="button" className="btn btn-success btn-sm" onClick={this.onClickHandler}>Upload </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
       </div>
     );
   }
