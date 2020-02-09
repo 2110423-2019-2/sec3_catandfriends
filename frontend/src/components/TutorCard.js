2bd509c088663a0e29551d8b1dec4a83906f3067
karnkittik <karnkitti_tang@hotmail.com>
2020-02-09 23:10:39 +0700

    complete tutorcard component

--- frontend/src/components/TutorCard.js
+++ frontend/src/components/TutorCard.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
-
+import "./TutorCard.css"
 export default class TutorCard extends Component {
     constructor(props) {
         super(props)
     
         this.state = {
              name:"Karnkitti Kittikamron",
-             contact:"@karnkittik",
              link:"/Karnkitti",
              imgsrc:"https://i.ibb.co/8NHMg4K/pic.png"
         }
@@ -14,11 +13,10 @@ export default class TutorCard extends Component {
     
     render() {
         return (
-            <div className="card" style={{width: "18rem"}}>
-                <img src={this.state.imgsrc} class="card-img-top" alt="..."/>
+            <div className="card " style={{width: "12rem", textAlign:"center"}}>
+                <img src={this.state.imgsrc} class="card-img-top" alt="..." />
                     <div class="card-body">
                         <h5 class="card-title">{this.state.name}</h5>
-                        <p class="card-text">{this.state.contact}</p>
                         <a href={this.state.link} class="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
