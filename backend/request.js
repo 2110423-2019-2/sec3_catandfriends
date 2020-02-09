ada1e03db9072e65f5fddd86816cfc3fe2f81be6
NatthanonM <natthanon.manop@gmail.com>
2020-02-09 18:36:42 +0700

    complete basic get and post

--- backend/request.js
+++ backend/request.js
@@ -1,16 +1,54 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose')
+const RequestModel = require('./models/request')
 
-router.get('/',(req,res) => {
-    res.json({});
+async function connection() {
+    try {
+        await mongoose.connect('mongodb+srv://User:User@cluster0-gcbxg.mongodb.net/TutorHere', { useNewUrlParser: true, useUnifiedTopology: true }
+        );
+        console.log("Successful connecting to database.");
+    } catch (error) {
+        console.log("Error connecting to database.");
+        throw "Conection Failed";
+    }
+}
+
+router.get('/', async (req, res) => {
+    // try {
+    // const connection = await connection();
+    connection();
+    const requests = await RequestModel.find({});
+    console.log(requests);
+    res.json(requests);
+    res.status(200).end();
+    // // } catch (error) {
+    //     if (error == "Conection Failed") {
+    //         res.send("Connect to database failed.");
+    //         res.status(200).end();
+    //     }
+    // }
 });
 
-router.post('/',(req,res)=>{
-
+router.post('/', async (req, res) => {
+    // try {
+    const payload = req.body;
+    const requests = new RequestModel(payload);
+    console.log(requests);
+    // const connection = await connection();
+    connection();
+    await requests.save();
+    res.status(201).end();
+    // // } catch (error) {
+    //     if (error == "Conection Failed") {
+    //         res.send("Connect to database failed.");
+    //         res.status(201).end();
+    //     }
+    // }
 });
 
-router.put('/',(req,res)=>{
-    
+router.put('/', (req, res) => {
+
 });
 
 
--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,6 @@ const scheduleRoute = require('./schedule');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 app.use('/courses',courseRoute);
 app.use('/requests',requestRoute);
 app.use('/profile',profileRoute);
