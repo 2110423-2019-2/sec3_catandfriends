dbe4f660d3ee7f8c7b4111745ebfe83d6fce3b90
karnkittik <karnkitti_tang@hotmail.com>
2020-02-11 14:09:55 +0700

    edit comment component

--- frontend/src/components/Comment.js
+++ frontend/src/components/Comment.js
@@ -15,14 +15,15 @@ export default class Comment extends Component {
     
     render() {
         return (
-            <div class="card mb-3 w-12" style={{maxWidth:430}}>
-            <div class="row no-gutters">
-              <div class="col-md-4">
-                <img src={this.state.imgsrc} class="card-img p-3"  alt="..."/>
+            <div class="card mb-3 w-12 innerbox">
+            <div class="row no-gutters ">
+              <div class="col-md-4 text-center ">
+                <img src={this.state.imgsrc} class="card-img p-2 pic"   alt="..."/>
               </div>
-              <div class="col-md-8">
+              <div class="col-md-8 ">
                 <div class="card-body">
-                <a class="card-title title" href={this.state.nameurl} >{this.state.name}</a>
+                  <center>
+                <a class="card-title title" href={this.state.nameurl}>{this.state.name}</a></center>
                 <p class="card-text">{this.state.comment}</p>
                 <p class="card-text"><small class="text-muted">Last updated {this.state.date}</small></p>
                 </div>
@@ -32,3 +33,5 @@ export default class Comment extends Component {
         )
     }
 }
+
+
