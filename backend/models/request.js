f4317c0cc446e044f360d7b068fea21be6581970
NatthanonM <natthanon.manop@gmail.com>
2020-02-13 03:38:37 +0700

    finish get request

--- backend/models/request.js
+++ backend/models/request.js
@@ -1,16 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 var collectionName = 'course_request';
+const moment = require('moment-timezone');
+const dateThailand = moment.tz(Date.now(), "Asia/Bangkok");
 
 const requestSchema = new Schema({
     requestId: String,
     courseId: String,
     tutorId: String,
     studentId: String,
-    status: Boolean,
-    timeStamp: { type: Date, default: Date.now }
+    status: { type: Boolean, default: false },
+    createdTime: { type: Date, default: dateThailand._d },
+    lastModified: { type: Date, default: dateThailand._d }
 }, {
-    collection: collectionName, versionKey: false  
+    collection: collectionName,
+    versionKey: false,
 });
 
 const RequestModel = mongoose.model('RequestModel', requestSchema);
--- backend/request.js
+++ backend/request.js
@@ -1,22 +1,58 @@
 const express = require('express');
 const router = express.Router();
 const RequestModel = require('./models/request')
+const to = require('await-to-js').default;
+const moment = require('moment-timezone');
+const dateThailand = moment.tz(Date.now(), "Asia/Bangkok");
 
 router.get('/', async (req, res) => {
-    let reqID = req.query.reqID;
-    console.log(reqID);
-    const requests = await RequestModel.find({});
-    console.log(requests);
+
+    let tutorID;
+    if (req.query.tutorID == undefined) {
+        res.status(400).end();
+        throw new Error('No argument error');
+    } else {
+        tutorID = req.query.tutorID.toString();
+    }
+    let err, requests;
+
+    [err, requests] = await to(RequestModel.find({
+        tutorId: tutorID
+    }));
+    if (err) {
+        res.status(500).end();
+        throw new Error('Unexpected error occurred');
+    }
+
     res.json(requests);
     res.status(200).end();
 });
 
 router.post('/', async (req, res) => {
     const payload = req.body;
+    let requestId = parseInt(dateThailand._d) + payload.tutorId;
+    
+    
+    payload.requestId = requestId;
+
+    console.log(payload);
     const requests = new RequestModel(payload);
     console.log(requests);
 
-    await requests.save();
+    let err, save;
+    // let condition = {
+    //     tutorId: payload.tutorId,
+    //     studentId: payload.studentId,
+    //     courseId: payload.courseId
+    // }
+
+    // [err, save] = await to(RequestModel.find(condition));
+
+    [err, save] = await to(requests.save());
+    if (err) {
+        res.status(500).end();
+        throw new Error('Unexpected error occurred');
+    }
     res.status(201).end();
 });
 
