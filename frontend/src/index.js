48c3ca2df2ed622efb7366bbb02eabd8890892c1
karnkittik <karnkitti_tang@hotmail.com>
2020-02-13 16:39:57 +0700

    show components

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import StudentCard from './components/StudentCard'
+import Titang from './components/Titang'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import * as serviceWorker from './serviceWorker';
+import Ice from './components/ice'
 
-ReactDOM.render(<StudentCard />, document.getElementById('root'));
+ReactDOM.render(<Titang />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
