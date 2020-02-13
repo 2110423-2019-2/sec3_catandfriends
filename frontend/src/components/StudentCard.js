82a2e048d437b230e26f09067c0df86b560c3e0a
karnkittik <karnkitti_tang@hotmail.com>
2020-02-13 16:00:31 +0700

    add phoneNumber studentcard

--- frontend/src/components/StudentCard.js
+++ frontend/src/components/StudentCard.js
@@ -11,7 +11,8 @@ export default class StudentCard extends Component {
       comment: "You know when you know when you know when you know you know",
       email: "@@@",
       faceurl: "/kkk",
-      bio: "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+      bio: "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+      phoneNumber: "19891989"
     };
   }
 
@@ -35,7 +36,7 @@ export default class StudentCard extends Component {
               <br/>
               <div className="row ">
                 <div className="col-md-6 border">
-                  <h4 style={{ textAlign: "center" }}>{this.state.name}</h4>
+                  <h4>{this.state.name}</h4>
                 </div>
                 <div className="col-md-6 border">
                   <h4>{this.state.surname}</h4>
@@ -44,7 +45,8 @@ export default class StudentCard extends Component {
               <br />
               <p className="card-text">{this.state.bio}</p>
               <a href={this.state.faceurl}>Facebook</a>
-              <p>Email: {this.state.email}</p>
+              <body>Email: {this.state.email}</body>
+              <body>Phone Number: {this.state.phoneNumber}</body>
               <p className="card-text">
                 <small className="text-muted">Last updated 3 mins ago</small>
               </p>
