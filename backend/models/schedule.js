c73cb7f25e62061483fb95e42581fe7c9d57114d
Pluemine <pluem_ultimate@hotmail.com>
2020-02-13 14:48:45 +0700

    added get/post/put backend schedulejs

---
+++ backend/models/schedule.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+var collectionName = 'schedule';
+
+const scheduleSchema = new Schema({
+    scheduleId: String,
+    studentId: String,
+    listOfCourse: Array
+}, {
+    collection: collectionName, versionKey: false  
+});
+
+const ScheduleModel = mongoose.model('ScheduleModel', scheduleSchema);
+
+module.exports = ScheduleModel;
