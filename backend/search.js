078370edd510355f3b2d7332b42b0eb2f0a096bb
natthanonM <natthanon.manop@gmail.com>
2020-02-15 16:44:47 +0700

    install module

--- backend/search.js
+++ backend/search.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const fuse = require('fuse.js');
 
 
-router.get('/', (req,res) => {
-    res.send("search");
+router.get('/', async (req, res) => {
+    let err, courses;
+
+    [err, courses] = await to(CourseModel.find({}));
+    if (err) {
+        res.status(500).end();
+    }
+
+    res.json(courses);
+    res.status(200).end();
 });
 
 
