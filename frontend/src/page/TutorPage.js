3785d50e8e09f53f481d5fe6b4e70991ba2e2ffb
karnkittik <karnkitti_tang@hotmail.com>
2020-02-17 18:30:17 +0700

    add tutorpage

---
+++ frontend/src/page/TutorPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import TutorProfile from '../components/TutorProfile'
+export default class TutorPage extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <TutorProfile/>
+                </div>
+            </div>
+        )
+    }
+}
