35aeefe39cfb120b23ceab1e181111a1398fea92
karnkittik <karnkitti_tang@hotmail.com>
2020-02-18 15:48:30 +0700

    edit tutorpage

--- frontend/src/page/SearchResult.js
+++ frontend/src/page/SearchResult.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react'
-import Filter from '../components/Filter'
-import CourseCard from '../components/CourseCard'
+import React, { Component } from "react";
+import Filter from "../components/Filter";
+import CourseCard from "../components/CourseCard";
 
 export default class SearchResult extends Component {
-    render() {
-        return (
-            <div className="row">
-                <div className="col-md-2">
-                <Filter/>
-                </div>
-                <div className="col-md-10">
-                <CourseCard/>
-                </div>
-            </div>
-        
-        )
-    }
+  state = { courses: [] };
+
+  render() {
+    return (
+      <div className="row">
+        <div className="col-md-2">
+          <Filter />
+        </div>
+        <div className="col-md-10">
+          <CourseCard />
+        </div>
+      </div>
+    );
+  }
 }
