a5ab1cc50c6d70acd81988b065091a01a7761315
karnkittik <karnkitti_tang@hotmail.com>
2020-02-18 17:48:27 +0700

    finish courselayout

--- frontend/src/components/CourseCardLayout.js
+++ frontend/src/components/CourseCardLayout.js
@@ -24,22 +24,52 @@ class CourseCardLayout extends React.Component {
           description: "TS",
           price: "4500",
           date: "Tuedays 8.00-10.00"
+        },
+        {
+          courseID: "00003",
+          courseName: "c",
+          image: "https://source.unsplash.com/user/erondu/600x400",
+          category: "more",
+          description: "TS",
+          price: "4550",
+          date: "Tuedays 8.00-10.00"
+        },
+        {
+          courseID: "00004",
+          courseName: "d",
+          image: "https://source.unsplash.com/user/erondu/600x400",
+          category: "more",
+          description: "TS",
+          price: "4300",
+          date: "Tuedays 8.00-10.00"
+        },
+        {
+          courseID: "00006",
+          courseName: "e",
+          image: "https://source.unsplash.com/user/erondu/600x400",
+          category: "more",
+          description: "TS",
+          price: "4600",
+          date: "Tuedays 8.00-10.00"
+        },
+        {
+          courseID: "00002",
+          courseName: "b",
+          image: "https://source.unsplash.com/user/erondu/600x400",
+          category: "more",
+          description: "TS",
+          price: "4500",
+          date: "Tuedays 8.00-10.00"
         }
-      ],
-      a: {
-        courseID: "00002",
-        courseName: "b",
-        image: "https://source.unsplash.com/user/erondu/600x400",
-        category: "more",
-        description: "TS",
-        price: "4500",
-        date: "Wednesdays 8.00-10.00"
-      }
+      ]
     };
   }
   render() {
     return (
-      <div className="row" style={{ maxWidth: "1100px" }}>
+      <div
+        className="row border"
+        style={{ maxWidth: "1100px", paddingLeft: "22px", marginTop: "22px" }}
+      >
         {this.state.rows.map(item => (
           <CourseCard detail={item} />
         ))}
