1cd51fd587c80d3107f003d98175cd522b3bf61f
natcha <natcha0012@gmail.com>
2020-03-07 13:27:57 +0700

    token

--- backend/profile.js
+++ backend/profile.js
@@ -1,9 +1,62 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const userModel = require("./models/user");
+const tutorModel = require("./models/tutor");
+const studentModel = require("./models/student");
 
-router.get('/',(req,res) => {
-
+router.get("/", async (req, res, next) => {
+  const userId = req.query.userId;
+  var profile;
+  if (!userId) {
+    profile = await userModel.findById(req.user._id);
+    profile.password = undefined;
+    switch (profile.role) {
+      case "tutor":
+        let tutorProfile = await tutorModel.findOne(
+          { userId: req.user._id },
+          { _id: 0, userId: 0 }
+        );
+        profile = { ...profile.toObject(), ...tutorProfile.toObject() };
+        break;
+      case "student":
+        let studentProfile = await studentModel.findOne(
+          { userId: req.user._id },
+          { _id: 0, userId: 0 }
+        );
+        profile = { ...profile.toObject(), ...studentProfile.toObject() };
+        break;
+    }
+    profile.owner = true;
+  } else {
+    profile = await userModel.findById(userId);
+    profile.password = undefined;
+    switch (profile.role) {
+      case "tutor":
+        let tutorProfile = await tutorModel.findOne(
+          { userId },
+          { _id: 0, userId: 0 }
+        );
+        profile = { ...profile.toObject(), ...tutorProfile.toObject() };
+        profile.email = undefined;
+        profile.ssn = undefined;
+        profile.premiumStatus = undefined;
+        profile.verificationDocument = undefined;
+        profile.owner = false;
+        break;
+      case "student":
+        let studentProfile = await studentModel.findOne(
+          { userId },
+          { _id: 0, userId: 0 }
+        );
+        profile = { ...profile.toObject(), ...studentProfile.toObject() };
+        profile.email = undefined;
+        profile.ssn = undefined;
+        profile.scheduleId = undefined;
+        profile.owner = false;
+        break;
+    }
+  }
+  return res.status(200).send(profile);
 });
-
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,54 @@
-const express = require('express');
+const express = require("express");
+const passport = require("passport");
 const app = express();
 const bodyParser = require("body-parser");
-const courseRoute = require('./course');
-const requestRoute = require('./request');
-const profileRoute = require('./profile');
-const scheduleRoute = require('./schedule');
-const mongoose = require('mongoose')
-mongoose.set('useCreateIndex', true);
-require('dotenv').config();
+const cors = require("cors");
+const loginRoute = require("./login");
+const signupRoute = require("./signup");
+const courseRoute = require("./course");
+const requestRoute = require("./request");
+const profileRoute = require("./profile");
+const scheduleRoute = require("./schedule");
+const searchRoute = require("./search");
+const uploadRoute = require("./upload");
+const mongoose = require("mongoose");
+require("./auth/auth");
+require("dotenv").config();
 
-mongoose.connect(process.env.MONGO_DB,{useUnifiedTopology: true, useNewUrlParser:true});
-mongoose.connection.on('error', err => {logError(err);});
-mongoose.connection.on('connected', () => {console.log("Connected")});
+mongoose.connect(process.env.MONGO_DB, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
+mongoose.connection.on("error", err => {
+  logError(err);
+});
+mongoose.connection.on("connected", () => {
+  console.log("Connected");
+});
 
-
+mongoose.set("useCreateIndex", true);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
-app.use('/courses', courseRoute);
-app.use('/requests', requestRoute);
-app.use('/profile', profileRoute);
-app.use('/schedule', scheduleRoute);
+app.use("/login", loginRoute);
+app.use("/signup", signupRoute);
+app.use("/courses",
+  passport.authenticate("jwt-profile", { session: false }),
+  courseRoute);
+app.use("/requests", requestRoute);
+app.use(
+  "/profile",
+  passport.authenticate("jwt-profile", { session: false }),
+  profileRoute
+);
+app.use("/schedule", scheduleRoute);
+app.use("/search", searchRoute);
+app.use(
+  "/upload",
+  passport.authenticate("jwt-profile", { session: false }),
+  uploadRoute
+);
 
 app.listen(8000, () => {
   console.log("Start server at port 8000.");
