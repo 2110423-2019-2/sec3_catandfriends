0f40420008dd68bfdfff3753688bf2f1026f03e9
karnkittik <karnkitti_tang@hotmail.com>
2020-03-09 18:33:45 +0700

    link courseinfo

--- frontend/src/components/CourseCard.js
+++ frontend/src/components/CourseCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./CourseCard.css";
+import history from "./../history";
 //import '@fortawesome/fontawesome-free';
 class CardHeader extends React.Component {
   render() {
@@ -60,17 +61,11 @@ class Button extends React.Component {
 class CardBody extends React.Component {
   constructor(props) {
     super(props);
-    this.handleOnClick = this.handleOnClick.bind(this);
+    //this.handleOnClick = this.handleOnClick.bind(this);
   }
-  handleOnClick(e) {}
   render() {
     return (
-      <div
-        className="mcard-body"
-        onClick={e => {
-          console.log("click");
-        }}
-      >
+      <div className="mcard-body">
         <article className="date">{this.props.date}</article>
         <h5 className="course-name">{this.props.title}</h5>
         <p className="body-content">{this.props.text}</p>
@@ -100,8 +95,12 @@ class CourseCard extends React.Component {
     const image =
       "https://i.kym-cdn.com/photos/images/newsfeed/001/535/446/1c5.jpg";
     const priceS = this.props.detail.courseFee + ".-";
+    const courseid = this.props.detail._id;
     return (
-      <article className="CourseCard">
+      <article
+        className="CourseCard"
+        onClick={() => this.onClickGotoCourseInform(courseid)}
+      >
         <CardHeader imgsrc={image} category={category}></CardHeader>
         <CardBody
           title={courseName}
@@ -113,6 +112,9 @@ class CourseCard extends React.Component {
       </article>
     );
   }
+  onClickGotoCourseInform = courseId => {
+    history.push(`/course?courseId=${courseId}`);
+  };
 }
 
 export default CourseCard;
