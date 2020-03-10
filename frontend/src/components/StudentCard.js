f4553e4f4add52d4a620f20d1115115258da224f
saint1998 <saint1998>
2020-03-10 23:42:01 +0700

    add view of tutor in student page(tutorSeeStudent)

--- frontend/src/components/StudentCard.js
+++ frontend/src/components/StudentCard.js
@@ -11,7 +11,8 @@ export default class StudentCard extends Component {
       comment: "You know when you know when you know when you know you know",
       email: "@@@",
       faceurl: "/kkk",
-      bio: "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+      bio:
+        "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
       phoneNumber: "19891989"
     };
   }
@@ -30,23 +31,26 @@ export default class StudentCard extends Component {
           </div>
           <div className="col-md-8">
             <div className="card-body">
-              <div className="row border mystyle text-center" className="mystyle">
+              <div
+                className="row border mystyle text-center"
+                className="mystyle"
+              >
                 <h3 className="card-title border">Student Profile</h3>
               </div>
-              <br/>
+              <br />
               <div className="row ">
                 <div className="col-md-6 border">
-                  <h4>{this.state.name}</h4>
+                  <h4>{this.props.data.firstName}</h4>
                 </div>
                 <div className="col-md-6 border">
-                  <h4>{this.state.surname}</h4>
+                  <h4>{this.props.data.lastName}</h4>
                 </div>
               </div>
               <br />
-              <p className="card-text">{this.state.bio}</p>
-              <a href={this.state.faceurl}>Facebook</a>
-              <body>Email: {this.state.email}</body>
-              <body>Phone Number: {this.state.phoneNumber}</body>
+              {/* <p className="card-text">{this.props.data.bio}</p>
+              <a href={this.props.data.faceurl}>Facebook</a>
+              <body>Email: {this.props.data.email}</body> */}
+              <body>Phone Number: {this.props.data.phoneNumber}</body>
               <p className="card-text">
                 <small className="text-muted">Last updated 3 mins ago</small>
               </p>
