494210e1697ec94fcda5298f615271fe77b6ca96
karnkittik <karnkitti_tang@hotmail.com>
2020-03-11 19:16:10 +0700

    add access constraint

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -11,48 +11,6 @@ export class NavBar extends Component {
     };
   }
 
-  // render() {
-  //   return (
-  //     <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
-  //       <a className="navbar-brand" href="#">
-  //         <img className="logoImg" src={this.state.imgjaa} alt="Logo" />
-  //       </a>
-  //       <h1>Tutor Here </h1>
-  //       <small>beta</small>
-  //       <div className="collapse navbar-collapse" id="navbarSupportedContent">
-  //         <ul className="navbar-nav mr-auto">
-  //           <li className="nav-item active">
-  //             <a class="nav-link" onClick={() => this.onClickNavBar("/")}>
-  //               Home <span class="sr-only">(current)</span>
-  //             </a>
-  //           </li>
-  //           <li className="nav-item active">
-  //             <a class="nav-link" onClick={() => this.onClickNavBar("/search")}>
-  //               Search <span class="sr-only">(current)</span>
-  //             </a>
-  //           </li>
-  //           <li className="nav-item active ">
-  //             <a
-  //               className="nav-link"
-  //               onClick={() => this.onClickNavBar(`/profile`)}
-  //             >
-  //               My profile <span className="sr-only">(current)</span>
-  //             </a>
-  //           </li>
-  //           <li className="nav-item active ">
-  //             <a
-  //               className="nav-link"
-  //               onClick={() => this.onClickNavBar("/logout")}
-  //             >
-  //               Log out <span className="sr-only">(current)</span>
-  //             </a>
-  //           </li>
-  //         </ul>
-  //       </div>
-  //     </nav>
-  //   );
-  // }
-
   render() {
     return (
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -123,9 +81,14 @@ export class NavBar extends Component {
     );
   }
   onClickNavBar = page => {
-    if (page == "/logout") {
+    if (page == "/register") {
+      history.push(page);
+    } else if (page == "/logout") {
       localStorage.clear();
       history.push("/");
+    } else if (!localStorage.getItem("token")) {
+      window.alert("Please login first!");
+      return history.push("./login");
     } else {
       history.push(page);
     }
