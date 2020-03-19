e2bc97f6e54de9f2ef2c48908cc000274b040604
natthanonM <natthanon.manop@gmail.com>
2020-03-19 18:37:43 +0700

    Fixed login

--- backend/auth/auth.js
+++ backend/auth/auth.js
@@ -68,7 +68,7 @@ passport.use(
           subject: "Verify Your Email",
           text: `Please click this link to verify your account\nhttp://localhost:8000/verify?email=${user.email}&hash=${hash}`
         };
-        transporter.sendMail(mailOptions, function(error, info) {
+        transporter.sendMail(mailOptions, function (error, info) {
           if (error) {
             return error;
           } else {
@@ -104,7 +104,7 @@ passport.use(
         const user = await userModel.findOne({ email });
         if (!user) {
           //If the user isn't found in the database, return a message
-          return done(null, false, { message: "User not found" });
+          return done({ message: "User not found" }, false, { message: "User not found" });
         }
         //Validate password and make sure it matches with the corresponding hash stored in the database
         //If the passwords match, it returns a value of true.
