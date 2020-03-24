5a9abdc2775727da56f9d4a07400b10ac516c38b
karnkittik <karnkitti_tang@hotmail.com>
2020-03-24 22:47:03 +0700

    Redesign editStudentProfile

--- frontend/src/components/EditTutorProfile.js
+++ frontend/src/components/EditTutorProfile.js
@@ -28,8 +28,8 @@ export default class EditTutorProfile extends Component {
 
   render() {
     return (
-      <div className="editTutorProfileCard border">
-        <h3 className="editProfileH border text-center">Edit Tutor Profile</h3>
+      <div className="editTutorProfileCard">
+        <h3 className="editProfileH text-center">Edit Tutor Profile</h3>
         <br />
         <form
           onSubmit={e => {
@@ -127,7 +127,11 @@ export default class EditTutorProfile extends Component {
           </div>
           <div className="row text-center" style={{ marginTop: "20px" }}>
             <div className="col-md-12">
-              <NormalButton color="green" type="submit" value="Submit">
+              <NormalButton
+                color="rgb(76, 182, 181)"
+                type="submit"
+                value="Submit"
+              >
                 Submit
               </NormalButton>
             </div>
