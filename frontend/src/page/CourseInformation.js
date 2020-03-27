d6a95ec9b5b9756795f548c99ea70dbb2eefc63c
karnkittik <karnkitti_tang@hotmail.com>
2020-03-27 21:09:55 +0700

    Course info

--- frontend/src/page/CourseInformation.js
+++ frontend/src/page/CourseInformation.js
@@ -8,11 +8,23 @@ export default class CourseInformation extends Component {
 
   render() {
     if (!this.state.data) {
-      return <div align="center">Loading...</div>;
+      return <div align="center"></div>;
     } else {
       return (
         <div className="justify-content-center">
-          <div className="row">
+          <div className="row justify-content-center">
+            <h2
+              style={{
+                padding: "5px 15px",
+                margin: "20px",
+                textAlign: "center",
+                color: "black",
+                backgroundColor: "rgba(255,255,255,0.3)",
+                borderRadius: "12px"
+              }}
+            >
+              {"Course: " + this.state.data.courseName}
+            </h2>
             <div className="col-md-12" align="center">
               <CourseDetail detail={this.state.data} />
             </div>
