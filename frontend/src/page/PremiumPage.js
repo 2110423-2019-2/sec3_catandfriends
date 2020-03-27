7e7e409880fd4d5d78abdcf8e81868f0dfe5d4b2
icewanee <iamicewanee@gmail.com>
2020-03-27 16:44:54 +0700

    create premiumpage

--- frontend/src/page/PremiumPage.js
+++ frontend/src/page/PremiumPage.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
+import PremiumCard from "../components/PremiumCard";
 
 export default class PremiumPage extends Component {
   render() {
     return (
-      <div className="bigCard border">
-        <div className="row">
-          <div className="col-md-12"></div>
-        </div>
+      <div className="col-md-12" align="center">
+        <PremiumCard />
       </div>
     );
   }
