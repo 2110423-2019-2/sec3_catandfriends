8b11991cabb9b0ff626052b56a9563a3dc6d295a
karnkittik <karnkitti_tang@hotmail.com>
2020-03-27 21:15:18 +0700

    Initial home page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,16 +1,17 @@
-
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class Home extends Component {
-    render() {
-        return (
-            <div>
-                Welcome to tutor here
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="justify-content-center">
+        <div className="row">
+          <div className="col-md-12" align="center">
+            <h1>This is Home</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Home
-
-
+export default Home;
