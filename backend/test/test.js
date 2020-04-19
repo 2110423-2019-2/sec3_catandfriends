93441da0ceb422f653bfdedc668fd931e49c3dab
natthanonM <natthanon.manop@gmail.com>
2020-04-19 18:16:56 +0700

    ADD toArray function to get data from env

--- backend/test/test.js
+++ backend/test/test.js
@@ -1,10 +1,17 @@
 var assert = require('assert');
 var request = require("request")
+require("dotenv").config();
+
+function toArray(string) {
+    const json = JSON.parse(string);
+    return json;
+}
 
 describe("## Comment API Tests", () => {
     describe("###GET /comment", () => {
-        it(`test1`, () => { assert(true, true) });
-        it(`test2`, () => { assert(true, false) });
+        const arrayTutorId = toArray(process.env.tutorId)
+        it(`test1`, () => { assert(345345, arrayTutorId[0]) });
+        it(`test2`, () => { assert(536456, arrayTutorId[1]) });
     });
 
     describe("###POST /requests/:token", () => {
