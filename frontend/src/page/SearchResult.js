6cf0475e8ddf1679fe3fad506557c954edb19572
karnkittik <karnkitti_tang@hotmail.com>
2020-04-19 11:24:48 +0700

    Fix home path and filter scrolling

--- frontend/src/page/SearchResult.js
+++ frontend/src/page/SearchResult.js
@@ -8,42 +8,11 @@ export default class SearchResult extends Component {
     super(props);
     this.state = {};
   }
-  onSearch = async data => {
+  onSearch = async (data) => {
     await this.setState({ data });
   };
   render() {
     if (!this.state.data) {
-      return (
-        <div>
-          <div className="row">
-            <div className="col-md-2">
-              <Filter search={this.onSearch} />
-            </div>
-            <div
-              className="col-md-10"
-              align="center"
-              style={{ maxWidth: "1200px" }}
-            >
-              <div className="row justify-content-center">
-                <div
-                className="inside-block textshadow"
-                  style={{
-                    marginTop: "20px",
-                    padding: "5px 15px",
-                    textAlign: "center",
-                    backgroundColor: "rgba(255,255,255,0.3)",
-                    borderRadius: "12px"
-                  }}
-                >
-                  {"Search Result: Loading..."}
-                </div>
-              </div>
-              <CourseCardLayout data={[]} />
-            </div>
-          </div>
-        </div>
-      );
-    } else {
       return (
         <div>
           <div className="row">
@@ -62,9 +31,40 @@ export default class SearchResult extends Component {
                     marginTop: "20px",
                     padding: "5px 15px",
                     textAlign: "center",
+                    backgroundColor: "rgba(255,255,255,0.3)",
+                    borderRadius: "12px",
+                  }}
+                >
+                  {"Search Result: Loading..."}
+                </div>
+              </div>
+              <CourseCardLayout data={[]} />
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <div className="row">
+            <div className="col-md-3">
+              <Filter search={this.onSearch} />
+            </div>
+            <div
+              className="col-md-9"
+              align="center"
+              style={{ maxWidth: "1200px" }}
+            >
+              <div className="row justify-content-center">
+                <div
+                  className="inside-block textshadow"
+                  style={{
+                    marginTop: "20px",
+                    padding: "5px 15px",
+                    textAlign: "center",
                     color: "black",
                     backgroundColor: "rgba(255,255,255,0.3)",
-                    borderRadius: "12px"
+                    borderRadius: "12px",
                   }}
                 >
                   {"Search Result: " +
